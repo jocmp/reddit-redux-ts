5c2d926d0199a8975eb564372d99d36e9ee5e7a9
Josiah Campbell <josiahcampbell64@gmail.com>
2017-05-10 17:12:06 -0400

    Update actions

--- src/actions.tsx
+++ src/actions.tsx
@@ -1,4 +1,4 @@
-import Subreddit from './models/Subreddit'
+import fetch from 'isomorphic-fetch'
 import Json from './models/Json'
 
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
@@ -6,28 +6,34 @@ export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 export const REQUEST_POSTS = 'REQUEST_POSTS'
 export const RECIEVE_POSTS = 'RECIEVE_POSTS'
 
-export const selectSubreddit = (subreddit: Subreddit) => {
+export const selectnumber = (subreddit: number) => {
     return {
         type: SELECT_SUBREDDIT, 
         subreddit
     }
 }
 
-export const invalidateSubreddit = (subreddit: Subreddit) => {
+export const invalidatenumber = (subreddit: number) => {
     return {
         type: INVALIDATE_SUBREDDIT,
         subreddit
     }
 } 
 
-export const requestPosts = (subreddit: Subreddit) => {
+export const fetchPosts = (subreddit: number) => {
+    return (dispatch: any) {
+        dispatch(requestPosts(subreddit))
+    }
+}
+
+export const requestPosts = (subreddit: number) => {
     return {
         type: REQUEST_POSTS,
         subreddit
     }
 }
 
-export const recievePosts = (subreddit: Subreddit, response: Json) => {
+export const recievePosts = (subreddit: number, response: Json) => {
     return {
         type: RECIEVE_POSTS,
         subreddit,
